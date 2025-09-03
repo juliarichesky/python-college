097b06712535d75b621032afd6cbdcb8bdae5345
julia :) <julificar1@gmail.com>
2025-09-03 16:32:03 -0300

    primeiro código em python

---
+++ 1-semestre/aula-01/index.py
@@ -0,0 +1,16 @@
+valor = input("Digite um número: ")
+numA = int(valor)
+valor = input("Digite um número: ")
+numB = int(valor)
+valor = input("Digite um número: ")
+numC = int(valor)
+valor = input("Digite um número: ")
+numD = int(valor)
+valor = input("Digite um número: ")
+numE = int(valor)
+valor = input("Digite um número: ")
+numF = int(valor)
+
+soma = numA + numB + numC + numD + numE
+
+print(soma)
