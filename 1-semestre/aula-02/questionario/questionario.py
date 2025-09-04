4ed70569154d61f390a2a0a0e3f6a32cf5a28b0e
julia :) <julificar1@gmail.com>
2025-09-04 10:12:24 -0300

    questionario

---
+++ 1-semestre/aula-02/questionario/questionario.py
@@ -0,0 +1,9 @@
+nome = input("Qual seu nome?")
+idade = input("Qual sua idade?")
+curso_superior = input("Curso superior? Qual?")
+escolha_TI = input("Por que escolheu TI?")
+hobby = input("Quais seus hobbies?")
+
+print(f"Olá {nome}. Você tem {idade} anos e nunca cursou {curso_superior}. O motivo de ter escolhido TI é {escolha_TI} e seus hobbies são {hobby}")
+
+#print(nome +"," + idade +"," + cursos + "," + ti + "," + hobbies)
