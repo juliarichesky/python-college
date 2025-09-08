f9ffd4362108dafb43df20eeced880beab17db39
julia :) <julificar1@gmail.com>
2025-09-08 09:22:44 -0300

    resolução exercicio 7

--- 1-semestre/aula-02/exercicios casa/ex007.py
+++ 1-semestre/aula-02/exercicios casa/ex007.py
@@ -1,1 +1,9 @@
 #uma pessoa tem em seu guarda roupa x camisas, y calças e z pares de sapato. escreva um algoritmo que calcula de quantas maneiras diferentes ele pode se vestir. seu algoritmo deverá ler o número de camisas, o número de calças e o número de pares de sapato.
+
+x = int(input("Digite o número de camisas: "))
+y = int(input("Digite o número de calças: "))
+z = int(input("Digite o número de pares de sapato: "))
+
+maneiras_de_se_vestir = x * y * z
+
+print(f"O número de maneiras de se vestir é: {maneiras_de_se_vestir}")
