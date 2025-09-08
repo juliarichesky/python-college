df8b51429af41a2ec835a956f1a311a586612ea5
julia :) <julificar1@gmail.com>
2025-09-08 11:24:48 -0300

    resolução exercício 4

--- 1-semestre/aula-02/exercicios casa/ex004.py
+++ 1-semestre/aula-02/exercicios casa/ex004.py
@@ -1,1 +1,10 @@
 #escreva um algoritmo que recebe um número x e y e imprime x^y. lembre-se que ∗∗ representa a potência entre dois números em python.
+
+x = float(input("Digite o valor de x: "))
+y = float(input("Digite o valor de y: "))
+
+#calcula x elevado a y
+resultado = x ** y
+
+#imprime o resultado
+print(f"{x} elevado a {y} é {resultado}")
