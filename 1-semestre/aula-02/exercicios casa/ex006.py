1631938b8dc3ed44f4cb6241d4fb8b04e6e9c03e
julia :) <julificar1@gmail.com>
2025-09-08 11:31:29 -0300

    resolução exercício 6

--- 1-semestre/aula-02/exercicios casa/ex006.py
+++ 1-semestre/aula-02/exercicios casa/ex006.py
@@ -1,1 +1,13 @@
 #sua tarefa é desenvolver um algoritmo que recebe um número inteiro de 0 a 99 e imprime o dígito das dezenas e o dígito das unidades desse número. dica: usando papel e lápis faça a divisão inteira do número por 10 mas não coloque vírgula e nem acrescente 0 na divisão.
+
+numero = int(input("Digite um número entre 0 e 99: "))
+
+
+#obtém o dígito das dezenas
+dezenas = numero // 10
+
+#obtém o dígito das unidades
+unidades = numero % 10
+
+print(f"Dígito das dezenas: {dezenas}")
+print(f"Dígito das unidades: {unidades}")
