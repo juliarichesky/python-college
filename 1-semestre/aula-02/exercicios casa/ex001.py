36e85b97f0d6df3b470508cbc9f58c4d56c07a10
julia :) <julificar1@gmail.com>
2025-09-08 11:07:13 -0300

    resolução exercicio 1

--- 1-semestre/aula-02/exercicios casa/ex001.py
+++ 1-semestre/aula-02/exercicios casa/ex001.py
@@ -1,1 +1,6 @@
 #escreva um algoritmo em python que recebe seu nome e sobrenome, depois de ler as informações mostra na tela o sobrenome e depois o nome.
+
+nome = input("Escreva seu nome: ")
+sobrenome = input("Escreva seu sobrenome: ")
+
+print(f"Olá {sobrenome}, {nome}")
