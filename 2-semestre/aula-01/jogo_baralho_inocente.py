9b04f9e3bb8e2781fcd7b30bce7891935181581d
julia :) <juliavaleriogs@gmail.com>
2026-02-09 09:55:02 -0300

    listas, jogo de baralho
    dicionário - listas, início do jogo de baralho

---
+++ 2-semestre/aula-01/jogo_baralho_inocente.py
@@ -0,0 +1,18 @@
+import baralho
+
+maco = baralho.cria('Normal')
+baralho.embaralha2(maco)
+
+pts_jog1 = 0
+pts_jog2 = 0
+
+for i in range(26):
+    c1 = baralho.compra(maco)
+    c2 = baralho.compra(maco)
+    if c1[0] < c2[0]:
+        pts_jog1 = pts_jog1 + 1
+    else:
+        pts_jog2 = pts_jog2 + 1
+
+print(f"Jogador 1: {pts_jog1}")
+print(f"Jogador 2: {pts_jog2}")
